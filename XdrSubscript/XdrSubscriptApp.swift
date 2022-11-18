a42bcaec53fb06f0a41a7b32d52fbc14751274ab
mm251134 <mm251134@ncr.com>
2022-11-18 14:09:17 +0100

    Initial Commit

---
+++ XdrSubscript/XdrSubscriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  XdrSubscriptApp.swift
+//  XdrSubscript
+//
+//  Created by Malovic, Milos on 18.11.22..
+//
+
+import SwiftUI
+
+@main
+struct XdrSubscriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
